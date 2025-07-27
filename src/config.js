a0a6da938494907504605ad8cbfa0c53d09e2905
Dark Creative <140174434+DarkCreative1@users.noreply.github.com>
2025-07-27 20:07:10 +0300

    Delete src directory

--- src/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  "prefix": "!",
-  "owner": "ID",
-  "token": "",
-  "ip": "play.craftrise.com",
-  "mode": "normal",//Aternos kullanıyorsanız aternos yazınız kullanmıyorsanız dokunmayın
-  "port": "25565",//Portunuz farklıysa değiştirin değilse kalsın
-}
